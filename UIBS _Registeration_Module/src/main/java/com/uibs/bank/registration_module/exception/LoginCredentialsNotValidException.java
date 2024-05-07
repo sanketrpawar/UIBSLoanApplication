2c0bc70963fea1957c9e6cbb0d8d821aaa6ea5e0
vyenktesh107 <165635414+vyenktesh107@users.noreply.github.com>
2024-05-07 11:32:14 +0530

    Delete UIBS _Registeration_Module directory

--- UIBS _Registeration_Module/src/main/java/com/uibs/bank/registration_module/exception/LoginCredentialsNotValidException.java
+++
@@ -1,8 +0,0 @@
-package com.uibs.bank.registration_module.exception;
-
-public class LoginCredentialsNotValidException extends RuntimeException {
-
-	public LoginCredentialsNotValidException(String msg) {
-		super(msg);
-	}
-}
--- UIBS _Registeration_Module/src/main/java/com/uibs/bank/registration_module/exception/RegistrationUnsuccessfulException.java
+++
@@ -1,8 +0,0 @@
-package com.uibs.bank.registration_module.exception;
-
-public class RegistrationUnsuccessfulException extends RuntimeException{
-
-	public RegistrationUnsuccessfulException(String msg) {
-		super(msg);
-	}
-}
